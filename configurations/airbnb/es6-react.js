231553434dc16424cc57e4d607786f6e674332dc
Eric Baer <me@ericbaer.com>
2016-04-13 15:12:25 -0700

    Remove configs external to walmart

--- configurations/airbnb/es6-react.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  "extends": [
-    "defaults/configurations/airbnb/es6",
-    "defaults/rules/react/airbnb"
-  ]
-};
--- configurations/airbnb/es6.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-module.exports = {
-  "extends": [
-    "defaults/configurations/airbnb/es5",
-    "defaults/rules/eslint/es6/airbnb"
-  ]
-};
