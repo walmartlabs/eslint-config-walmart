563230d2223330dcbbdcd49e31ab47f37234d573
Eric Baer <me@ericbaer.com>
2016-06-08 09:48:28 -0700

    Update configuration to define node in es6 as script and not module

---
+++ configurations/es6-node-test.js
@@ -0,0 +1,19 @@
+"use strict";
+
+module.exports = {
+  "extends": [
+    "defaults/configurations/es6-node"
+  ],
+  "env": {
+    "mocha": true,
+    "phantomjs": true
+  },
+  "globals": {
+    "expect": true,
+    "sandbox": true
+  },
+  "rules": {
+    "max-nested-callbacks": 0,
+    "no-unused-expressions": 0
+  }
+};
