dfc274600858bb397ea1077f2c6d6f4ec84b6b06
Eric Baer <me@ericbaer.com>
2016-06-08 10:04:46 -0700

    Correct lint errors

--- rules/filenames/on.js
+++ rules/filenames/on.js
@@ -4,8 +4,9 @@ module.exports = {
   "plugins": [
     "filenames"
   ],
+  "rules": {
     // Enforce dash-cased filenames
-    "filenames/match-regex": [2, '^[a-z0-9\\-\\.]+$', true],
+    "filenames/match-regex": [2, "^[a-z0-9\\-\\.]+$", true],
     // Match the file name against the default exported value in the module
     "filenames/match-exported": 0,
     // Don't allow index.js files
