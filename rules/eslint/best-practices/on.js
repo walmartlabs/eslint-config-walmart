0903d81cc0297f527a4b031ee58da7924a4ae8c6
Mark Brouch <mbrouch@walmartlabs.com>
2016-09-30 11:48:01 -0500

    🔧 Ignore basic numbers in no-magic-numbers rule
    Ignoring `-1`, `0` and `1` in the `no-magic-numbers` rule allows iterators, length comparators,
    and expect quantities to exist without throwing lint errors.

--- rules/eslint/best-practices/on.js
+++ rules/eslint/best-practices/on.js
@@ -69,7 +69,7 @@ module.exports = {
     // disallow creation of functions within loops
     "no-loop-func": 2,
     // disallow the use of magic numbers
-    "no-magic-numbers": 2,
+    "no-magic-numbers": [2, {"ignore": [-1, 0, 1]}],
     // disallow use of multiple spaces
     "no-multi-spaces": 2,
     // disallow use of multiline strings
