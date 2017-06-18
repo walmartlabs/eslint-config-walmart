17d553fdfba87eb7d8b1e96e9cd3122ee292e562
Sheng Di <sdi@usc.edu>
2017-06-17 20:08:21 -0700

    fix deprecated wrap multilines (#29)

--- rules/react/off.js
+++ rules/react/off.js
@@ -46,7 +46,7 @@ module.exports = {
     // Enforce propTypes declarations alphabetical sorting
     "sort-prop-types": 0,
     // Prevent missing parentheses around multilines JSX
-    "react/wrap-multilines": 0,
+    "react/jsx-wrap-multilines": 0,
 
     // ========================================================================
     //                                JSX Specific Rules
